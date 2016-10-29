57a82688110c23cf16b78303627264417695b906
David Cohen <dacohenii@gmail.com>
2016-10-29 19:07:59 -0400

    added venue list

---
+++ venuelist.js
@@ -0,0 +1,59 @@
+let rp = require('request-promise');
+let cheerio = require('cheerio');
+
+let VENUES = {
+  "tct": {
+    "name": "The Community Tap",
+    "uri": "http://thecommunitytap.com",
+    parse_beer_list: ($) => {
+      return $("div.beer", "div.view-pouring-now.view-display-id-beers_tap")
+        .map((ndx, ele) => ({
+          'brewery': $(ele).children('.brewery').text().trim(),
+          'name' : $(ele).clone().children().remove().end().text().trim(),
+          'urls' : $(ele)
+            .children('.brewery-links-container')
+            .children('.brewery-links')
+            .children('.link')
+            .map((n, e) => $(e).attr('data-url'))
+            .get()
+            .filter((ele) => typeof ele === 'string' && ele !== '')
+        }))
+        .get()
+    }
+  }
+};
+
+
+function getVenues(options, callback){
+  let p = new Promise(function(resolve=callback, reject){
+
+    let res = Object.keys(VENUES).map((key) => ({
+      "id": key,
+      "name": VENUES[key].name,
+      "uri": VENUES[key].uri,
+    }))
+
+    resolve(res);
+  });
+
+  return p;
+}
+
+
+function getVenueDetails(venueID, callback){
+  let v = VENUES[venueID];
+  let options = {
+    uri: v.uri,
+    transform: function(body){
+      return cheerio.load(body);
+    }
+  };
+  
+  rp(options)
+    .then(function($){
+      callback(v.parse_beer_list($));
+    });
+
+}
+
+module.exports = { getVenues, getVenueDetails };
